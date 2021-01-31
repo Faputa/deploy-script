fd051d0e1f56842c1136efebd441e831055363b2
refcell <2238932729@qq.com>
2021-01-31 19:43:59 +0800

    因为Linux不会出现目录和文件同名的情况，所以去掉冗余判断

--- status.sh
+++ status.sh
@@ -3,9 +3,7 @@
 d0=$(dirname $0)
 
 for d1 in $(ls $d0); do
-  if test -d $d0/$d1; then
-    if test -f "$d0/$d1/status.sh"; then
-      sh $d0/$d1/status.sh
-    fi
+  if test -f "$d0/$d1/status.sh"; then
+    sh $d0/$d1/status.sh
   fi
 done
