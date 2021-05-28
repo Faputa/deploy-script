a69ae821a002a42fdb075362f7b8b215625e6e62
refcell <2238932729@qq.com>
2021-05-28 15:54:24 +0800

    使用绝对路径

--- demo/start.sh
+++ demo/start.sh
@@ -1,4 +1,4 @@
 app=demo-1.0.0.jar
-d0=$(dirname $0)
+d0=$(cd $(dirname $0) && pwd)
 # nohup java -jar $d0/$app >/dev/null 2>&1 &
 nohup java -jar $d0/$app >$d0/${app/%.jar/.log} 2>&1 &
--- stop.sh
+++ stop.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-d0=$(dirname $0)
+d0=$(cd $(dirname $0) && pwd)
 
 for d1 in $(ls $d0); do
   if test -f "$d0/$d1/stop.sh"; then
